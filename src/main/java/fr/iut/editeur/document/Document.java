217b9cd3bfb011386c1691087014d9ba33322858
Galdric Desert-Caparros <galdric.desert-caparros@etu.umontpellier.fr>
2024-09-08 11:40:20 +0200

    refactor : ajout de documentation

--- src/main/java/fr/iut/editeur/document/Document.java
+++ src/main/java/fr/iut/editeur/document/Document.java
@@ -16,24 +16,47 @@ public class Document {
         this.texte = texte;
     }
 
+    /**
+     * Ajoute du texte à la fin du document
+     * @param texte texte à ajouter
+     */
     public void ajouter(String texte) {
         this.texte += texte;
     }
 
+    /**
+     * Remplace le texte entre les positions debut et fin par le texte remplacement
+     * @param debut position de début
+     * @param fin position de fin
+     * @param remplacement texte de remplacement
+     */
     public void remplacer(int debut, int fin, String remplacement) {
         String partieGauche = texte.substring(0, debut);
         String partieDroite = texte.substring(fin + 1);
         texte = partieGauche + remplacement + partieDroite;
     }
 
+/**
+     * Met en majuscule le texte entre les positions debut et fin
+     * @param debut position de début
+     * @param fin position de fin
+     */
     public void majuscules(int debut, int fin) {
         remplacer(debut, fin, texte.substring(debut, fin + 1).toUpperCase());
     }
 
+    /**
+     * Efface le texte entre les positions debut et fin
+     * @param debut position de début
+     * @param fin position de fin
+     */
     public void effacer(int debut, int fin) {
         remplacer(debut, fin, "");
     }
 
+    /**
+     * Efface tout le texte
+     */
     public void clear() {
         texte = "";
     }
